023392a3559ee7897c3ad5972c140ff33b9c1342
abarfourdotinfinity <abar@fourdotinfinity.com>
2025-02-20 09:30:32 +0200

    json fix

--- hyperparameter_generator.py
+++ hyperparameter_generator.py
@@ -5,15 +5,15 @@ import json
 parser = argparse.ArgumentParser(description='Process some integers.')
 
 # Add the arguments
-parser.add_argument('--BATCH_SIZE', type=int,default=64)
+parser.add_argument('--BATCH_SIZE', type=int,default=16)
 parser.add_argument('--FILL_NAN', type=int,default=10)
 parser.add_argument('--TIME_STEP', type=int, default=10)
-parser.add_argument('--COLUMN', type=str, default='cpu_consumption')
-parser.add_argument('--EPOCHS', type=int, default=2000)
-parser.add_argument('--lr', type=float,default=0.001)
-parser.add_argument('--HIDDEN_LAYER_SIZE', type=int, default=10)
-parser.add_argument('--NUM_LAYERS', type=int,default=3)
-parser.add_argument('--TRAIN_TEST_SPLIT', type=float,default=0.9)
+parser.add_argument('--COLUMN', type=str, default='flow')
+parser.add_argument('--EPOCHS', type=int, default=100)
+parser.add_argument('--lr', type=float,default=0.0001)
+parser.add_argument('--HIDDEN_LAYER_SIZE', type=int, default=100)
+parser.add_argument('--NUM_LAYERS', type=int,default=5)
+parser.add_argument('--TRAIN_TEST_SPLIT', type=float,default=0.875)
 parser.add_argument('--ROOT_FOLDER', type=str,default='.')
 
 # Parse the arguments
